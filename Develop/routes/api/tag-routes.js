e8f1456f6e7861296d75e98ddefcf8f93f203b2c
AJ Jaswal <jaswalakshay@yahoo.com>
2022-06-26 10:49:01 -0400

    delete tag by ID tag-routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -86,11 +86,34 @@ router.put('/:id', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
-    
+
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+
+  // delete tag by ID
+  Tag.remove({
+    where: {
+
+      id: req.params.id
+
+    }
+  })
+  .then(dbTagData => {
+    if (!dbTagData) {
+
+      res.status(404).json({message: 'No tag found with this id'});
+      return;
+
+    }
+    res.json(dbTagData);
+  })
+
+  .catch(err =>{
+    console.log(err);
+    res.status(500).json(err);
+  });
+
 });
 
 module.exports = router;
