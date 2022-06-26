95ba9dc41d7585e96a4a7f0f415304d94cf2cbb9
AJ Jaswal <jaswalakshay@yahoo.com>
2022-06-26 10:29:55 -0400

    find one category by ID

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -4,7 +4,7 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  
+
   Category.findAll({
     include: {
 
@@ -28,8 +28,30 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+Category.findOne({
+    where: {
+      id: req.params.id
+    },
+
+    include: {
+
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+
+    }
+  })
+    .then(dbCatData => {
+      if(!dbCatData) {
+        res.status(404).json({message: 'No category found'});
+        return;
+      }
+      res.json(dbCatData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err)
+    });
+
 });
 
 router.post('/', (req, res) => {
