f771492db36d18f89ec848f9c052390e204eb369
AJ Jaswal <jaswalakshay@yahoo.com>
2022-06-26 10:44:24 -0400

    tag-routes find all tags

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -2,10 +2,23 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-
+// Find all tags
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    include: {
+
+      model: Product,
+      attributes: ['product_name', 'price', 'stock', 'category_id']
+
+    }
+  })
+
+    .then(dbTagData => res.json(dbTagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+
 });
 
 router.get('/:id', (req, res) => {
